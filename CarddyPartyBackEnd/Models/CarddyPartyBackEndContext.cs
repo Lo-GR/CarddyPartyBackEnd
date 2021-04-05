5beb2d83a9e5c5af1f0104dd23113ebc73ff51f2
Lo-GR <diamondintheroth@gmail.com>
2021-04-05 14:53:39 -0700

    second migration fix seeding
    Co-authored-by: Ash Porter <porter.ashley13@gmail.com>

--- CarddyPartyBackEnd/Models/CarddyPartyBackEndContext.cs
+++ CarddyPartyBackEnd/Models/CarddyPartyBackEndContext.cs
@@ -19,16 +19,18 @@ namespace CarddyPartyBackEnd.Models
     protected override void OnModelCreating(ModelBuilder builder)
     {
       builder.Entity<Card>().HasData(
-        new Card {CardId = 1, Answer = "Card One"},
-        new Card {CardId = 2, Answer = "Card Two"},
-        new Card { CardId = 3, Answer = "Card Three" },
-        new Card { CardId = 4, Answer = "Card Four" },
-        new Card { CardId = 5, Answer = "Card Five" },
-        new Card { CardId = 6, Answer = "Card Six" },
-        new Card { CardId = 7, Answer = "Card Seven" },
-        new Card { CardId = 8, Answer = "Card Eight" },
-        new Card { CardId = 9, Answer = "Card Nine" },
-        new Card { CardId = 10, Answer = "Card Ten" }
+        
+
+        new Card {CardId = 1, Answer = "Card One", PlayerId = 1},
+        new Card {CardId = 2, Answer = "Card Two", PlayerId = 2},
+        new Card { CardId = 3, Answer = "Card Three", PlayerId = 1 },
+        new Card { CardId = 4, Answer = "Card Four", PlayerId = 2 },
+        new Card { CardId = 5, Answer = "Card Five", PlayerId = 1 },
+        new Card { CardId = 6, Answer = "Card Six", PlayerId = 2 },
+        new Card { CardId = 7, Answer = "Card Seven", PlayerId = 1 },
+        new Card { CardId = 8, Answer = "Card Eight", PlayerId = 2 },
+        new Card { CardId = 9, Answer = "Card Nine", PlayerId = 1 },
+        new Card { CardId = 10, Answer = "Card Ten", PlayerId = 2 }
       );
     }
   }
