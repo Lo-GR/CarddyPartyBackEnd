8877ff27392ef5bb72d4ad245882cf9168b5486f
Lo-GR <diamondintheroth@gmail.com>
2021-04-05 11:17:32 -0700

    commit rest of project
    Co-authored-by: Ash Porter <porter.ashley13@gmail.com>
    Co-authored-by: Ash Porter <porter.ashley13@gmail.com>
    Co-authored-by: Dani Renner <danijrenner@gmail.com>
    Co-authored-by: Nick LIndau <nicholaithegreat@gmail.com>

---
+++ CarddyPartyBackEnd/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+// using CarddyPartyBackEnd.Models;
+using System;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarddyPartyBackEnd
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<CarddyPartyBackEndContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+            services.AddControllers();
+            services.AddApiVersioning(o => {
+            o.ReportApiVersions = true;
+            o.AssumeDefaultVersionWhenUnspecified = true;
+            o.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Carddy Party",
+                    Description = "Bacardi pls no sue",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Logan Roth (Lo-GR), Ash Porter, Dani Renner, Nick Lindau",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/Lo-GR/CarddyPartyBackEnd"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT, (c) Logan Roth, Ash Porter, Dani Renner, Nick Lindau, 2021",
+                        Url = new Uri("https://opensource.org/licenses/MIT"),
+                    }
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Carddy Party");
+            });
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+        
+    }
+}
