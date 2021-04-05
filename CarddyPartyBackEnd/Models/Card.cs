f2386826062904424111bcc115bdc5da6204e128
Nick Lindau <nicholaithegreat@gmail.com>
2021-04-05 13:48:54 -0700

    create context and player/card/prompt classes Co-authored-by: Dani Renner <danijrenner@gmail.com>

---
+++ CarddyPartyBackEnd/Models/Card.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace CarddyPartyBackEnd.Models
+{
+  public class Card
+  {
+    public int CardId { get; set; }
+    [Required]
+    public string Answer { get; set; }
+    public virtual Player player { get; set; }
+  }
+  
+}
