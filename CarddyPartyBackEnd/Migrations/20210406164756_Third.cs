f2a61b31eee5d2fe4e2e425a6b1f81de53550e51
Lo-GR <diamondintheroth@gmail.com>
2021-04-06 09:49:00 -0700

    add third migration
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

---
+++ CarddyPartyBackEnd/Migrations/20210406164756_Third.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarddyPartyBackEnd.Migrations
+{
+    public partial class Third : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Theme",
+                table: "Prompts",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Prompts",
+                keyColumn: "PromptId",
+                keyValue: 1,
+                column: "Theme",
+                value: "politics");
+
+            migrationBuilder.UpdateData(
+                table: "Prompts",
+                keyColumn: "PromptId",
+                keyValue: 2,
+                column: "Theme",
+                value: "jokes");
+
+            migrationBuilder.UpdateData(
+                table: "Prompts",
+                keyColumn: "PromptId",
+                keyValue: 3,
+                column: "Theme",
+                value: "epicodus");
+
+            migrationBuilder.InsertData(
+                table: "Prompts",
+                columns: new[] { "PromptId", "Description", "Theme" },
+                values: new object[,]
+                {
+                    { 4, "Who had the worst scandal?", "politics" },
+                    { 5, "Why did my dad leave me?", "jokes" },
+                    { 6, "Epicodus is terrible at", "epicodus" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Prompts",
+                keyColumn: "PromptId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Prompts",
+                keyColumn: "PromptId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Prompts",
+                keyColumn: "PromptId",
+                keyValue: 6);
+
+            migrationBuilder.DropColumn(
+                name: "Theme",
+                table: "Prompts");
+        }
+    }
+}
